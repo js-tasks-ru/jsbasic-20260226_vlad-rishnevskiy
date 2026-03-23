c797def5420e3e6c388850cdc7539079df018e0d
vr <vr@paykeeper.ru>
2026-03-23 12:13:33 +0200

    fix checkSpam function

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,5 +2,5 @@ function truncate(str, maxlength) {
   if (str.length <= maxlength) {
     return str;
   }
-  return str.slice(0, maxlength - 3) + '...';
+  return str.slice(0, maxlength - 3) + "…";
 }
