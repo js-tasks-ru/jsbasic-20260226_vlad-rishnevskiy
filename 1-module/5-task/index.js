d696c6cd337e090608ae6cd17414d8f790631a56
vr <vr@paykeeper.ru>
2026-03-30 13:04:10 +0300

    fix tasks

--- 1-module/5-task/index.js
+++ 1-module/5-task/index.js
@@ -2,5 +2,5 @@ function truncate(str, maxlength) {
   if (str.length > maxlength) {
     return str.slice(0, maxlength - 1) + '…';
   }
-  return str.slice(0, maxlength - 3) + "…";
+  return str;
 }
