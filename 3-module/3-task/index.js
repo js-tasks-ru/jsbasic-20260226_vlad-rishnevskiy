9f1ed011fad07ab8e7377b2d27df58da3bfc7f8f
vr <vr@paykeeper.ru>
2026-04-05 13:25:45 +0300

    fix all

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,9 @@
 function camelize(str) {
-  // ваш код...
+  return str
+    .split('-')
+    .map((word, index) => {
+      if (index === 0) return word;
+      return word.charAt(0).toUpperCase() + word.slice(1);
+    })
+    .join('');
 }
