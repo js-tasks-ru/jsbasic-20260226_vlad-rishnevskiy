b5fb1681ee510022f1081e4e846f37a32e21808c
vr <vr@paykeeper.ru>
2026-04-05 13:21:46 +0300

    fix 4 module

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,33 @@
 function highlight(table) {
-  // ваш код...
+  const rows = table.querySelectorAll('tbody tr');
+
+  rows.forEach(row => {
+    const cells = row.querySelectorAll('td');
+
+    const age = Number(cells[1].textContent);
+    const gender = cells[2].textContent;
+    const statusCell = cells[3];
+
+    if (!statusCell.hasAttribute('data-available')) {
+      row.hidden = true;
+    } else {
+      const available = statusCell.dataset.available;
+
+      if (available === 'true') {
+        row.classList.add('available');
+      } else {
+        row.classList.add('unavailable');
+      }
+    }
+
+    if (gender === 'm') {
+      row.classList.add('male');
+    } else if (gender === 'f') {
+      row.classList.add('female');
+    }
+
+    if (age < 18) {
+      row.style.textDecoration = 'line-through';
+    }
+  });
 }
